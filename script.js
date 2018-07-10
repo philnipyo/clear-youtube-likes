3cbed0e46b9f01f4f9e3b10e5071ac52f5aa6841
philnipyo <philnipyo@yahoo.com>
2018-07-09 17:48:51 -0700

    Select Youtube videos by their class name and append checkbox inputs for each video

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 // Select all table rows containing your liked Youtube videos
-let videos = document.querySelectorAll('tr');
+// querySelectorAll returns a NodeList instead of an array; however, NodeLists can still use forEach
+let videos = document.getElementsByClassName('pl-video yt-uix-tile');
 
 // Insert a checkbox for each <tr>
-videos.forEach(function(el){
-    el.insertAdjacentHTML('afterend', '<input type="checkbox">');
-});
+// videos.forEach(function(el){
+//     el.insertAdjacentHTML('afterend', '<input type="checkbox">');
+// });
+
+for(let h = 0; h < videos.length; h++){
+    // videos[h].insertAdjacentHTML('beforeend', '<input type="checkbox">');
+    videos[h].appendChild('beforeend', '<input type="checkbox">');
+}
