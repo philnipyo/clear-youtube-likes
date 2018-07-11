aec38a8ab9fd904b5597e3c51a844b6ace5b5d53
philnipyo <philnipyo@yahoo.com>
2018-07-10 22:51:14 -0700

    Add loop to bulk remove liked videos; make note of how Chrome and Youtube get after doing so

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-// Select all table rows containing your liked Youtube videos
-// querySelectorAll returns a NodeList instead of an array; however, NodeLists can still use forEach
+// Select all the currently displayed liked videos playlist
 let videos = document.getElementsByClassName('pl-video yt-uix-tile');
 
-// Insert a checkbox for each <tr>
-// videos.forEach(function(el){
-//     el.insertAdjacentHTML('afterend', '<input type="checkbox">');
-// });
+// Select all the remove buttons for each liked video in the playlist
+let removeButtons = document.getElementsByClassName('pl-video-edit-remove-liked-video');
 
+// Loop through each video in the Nodelist and append a child checkbox input element before the end of each video
 for(let h = 0; h < videos.length; h++){
-    // videos[h].insertAdjacentHTML('beforeend', '<input type="checkbox">');
     videos[h].appendChild('beforeend', '<input type="checkbox">');
+}
+
+// For each close button in removeButtons, click the button. This will bulk delete all liked videos.
+// However, Youtube becomes unresponsive and makes Google Chrome lag when you try to do other things.
+
+// Loop through and click all of the remove liked video buttons 
+for(let i = 0; i < removeButtons.length; i++){
+    removeButtons[i].click();
 }
