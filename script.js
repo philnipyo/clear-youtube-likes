2dc85f25f7f7c85a5e2f4515238c5b50b99f1f32
philnipyo <philnipyo@yahoo.com>
2018-07-11 17:35:29 -0700

    Replace appendChild method with insertAdjacentHTML method

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ let videos = document.getElementsByClassName('pl-video yt-uix-tile');
 // Select all the remove buttons for each liked video in the playlist
 let removeButtons = document.getElementsByClassName('pl-video-edit-remove-liked-video');
 
-// Loop through each video in the Nodelist and append a child checkbox input element before the end of each video
+// Loop through each video in the Nodelist and insert a checkbox before the end of each video tile
 for(let h = 0; h < videos.length; h++){
-    videos[h].appendChild('beforeend', '<input type="checkbox">');
+    videos[h].insertAdjacentHTML('beforeend', '<input type="checkbox">');
 }
 
 // For each close button in removeButtons, click the button. This will bulk delete all liked videos.
